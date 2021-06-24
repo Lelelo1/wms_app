f24fff62bef0911e54762cb84d8e2cd36a2fb3d1
Lelelo1 <lolole51@hotmail.com>
2021-06-24 10:48:56 +0200

    remove CameraPreview dependency and restore initial android minsdk

--- lib/pages/prototypes/plockPageIdeal.dart
+++ lib/pages/prototypes/plockPageIdeal.dart
@@ -5,48 +5,21 @@ import 'package:camera/camera.dart';
 
 class PlockPageIdeal extends StatefulWidget {
   @override
-  _CameraAppState createState() => _CameraAppState();
+  _State createState() => _State();
 }
 
-class _CameraAppState extends State<PlockPageIdeal> {
+class _State extends State<PlockPageIdeal> {
   @override
   void initState() {
     super.initState();
-    initialize();
   }
-
-  void initialize() async {
-    var cameras = await availableCameras();
-    if (cameras?.length == 0) {
-      print("there where no cameras on this device");
-      cameraControllerCompleter.complete(null);
-      return null;
-    }
-
-    var controller = CameraController(cameras[0], ResolutionPreset.high);
-    await controller.initialize();
-    cameraControllerCompleter.complete(controller);
-  }
-
-  Completer<CameraController> cameraControllerCompleter = Completer();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<CameraController>(
-        future: cameraControllerCompleter.future,
-        builder:
-            (BuildContext context, AsyncSnapshot<CameraController> snapshot) {
-          var controller = snapshot.data;
+    return cameraScanning();
+  }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // happens first run
-            return Container();
-          }
-
-          if (controller == null) {
-            return Text("Ops, something went wrong with cameras :(");
-          }
-          return CameraPreview(controller);
-        });
+  cameraScanning() {
+    return Container();
   }
 }
