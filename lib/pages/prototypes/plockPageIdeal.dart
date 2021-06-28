3d6c0f77a73b786afcd97ff9793a870dbf208284
Lelelo1 <lolole51@hotmail.com>
2021-06-28 11:28:01 +0200

    top ideal plock scanning button sized correctly

--- lib/pages/prototypes/plockPageIdeal.dart
+++ lib/pages/prototypes/plockPageIdeal.dart
@@ -6,6 +6,7 @@ class PlockPageIdeal extends StatefulWidget {
 }
 
 class _State extends State<PlockPageIdeal> {
+  MediaQueryData mediaQueryData;
   @override
   void initState() {
     super.initState();
@@ -13,22 +14,41 @@ class _State extends State<PlockPageIdeal> {
 
   @override
   Widget build(BuildContext context) {
+    if (mediaQueryData == null) {
+      mediaQueryData = MediaQuery.of(context);
+    }
     return Container(child: Column(children: [top()]));
   }
 
   Widget top() {
+    var topHeightFactor = 0.31;
+    var topHeight = mediaQueryData.size.height * topHeightFactor;
+    print("top is: " + mediaQueryData.padding.top.toString());
+    var statusBarHeight = mediaQueryData.padding.top;
+
+    var buttonWidthFactor = 0.65;
+    var buttonWidth = mediaQueryData.size.width * buttonWidthFactor;
+    var buttonHeightFactor = 0.08;
+    var buttonHeight = mediaQueryData.size.height * buttonHeightFactor;
+
     return Container(
         child: Center(
-            child: ButtonTheme(
-                child: ElevatedButton(
-                    onPressed: Scan, child: Icon(Icons.qr_code_scanner)),
-                height: 200,
-                minWidth: 300)),
-        height: 300,
-        color: Colors.grey);
+          child: SizedBox(
+            child: MaterialButton(
+                child: Icon(Icons.qr_code),
+                onPressed: scan,
+                color: Color.fromARGB(180, 133, 57, 227)),
+            width: buttonWidth,
+            height: buttonHeight,
+          ),
+        ),
+        color: Colors.grey,
+        height: topHeight,
+        margin: EdgeInsets.only(
+            left: 0, top: statusBarHeight, right: 0, bottom: 0));
   }
 
-  void Scan() {
+  void scan() {
     print("scan");
   }
 }
