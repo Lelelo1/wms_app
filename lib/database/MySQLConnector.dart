d3cbc784dded4b1b57a6c0789c41ef499728e22f
Lelelo1 <lolole51@hotmail.com>
2021-07-01 14:14:02 +0200

    moved products source to database folder, create code that creates mysl databse connection settings

---
+++ lib/database/MySQLConnector.dart
@@ -0,0 +1,25 @@
+import 'package:mysql1/mysql1.dart';
+import 'dart:io' show Platform;
+
+// https://pub.dev/packages/mysql1
+class MySQLConnector {
+  static ConnectionSettings settings;
+
+  static ConnectionSettings createConnectionSettings() {
+    var password =
+        Platform.environment["WMSAppTestingSQLDatabseConnectionPassword"];
+    return new ConnectionSettings(
+        host: '127.0.0.1',
+        port: 3306,
+        user: "lelelo1",
+        password: password,
+        db: "sakila");
+  }
+
+  static void Test() {
+    if (settings == null) {
+      settings = createConnectionSettings();
+      print("created database connection settings");
+    }
+  }
+}
