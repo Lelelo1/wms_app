4cde8d078e1a9accf6f5de4c8f6a045c2e4bc5c4
Lelelo1 <lolole51@hotmail.com>
2021-11-19 12:34:10 +0100

    ios changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,13 +3,15 @@ import Flutter
 import Firebase
 
 @UIApplicationMain
+
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    FirebaseApp.configure()
+    // FirebaseApp.configure() https://stackoverflow.com/questions/65154152/unable-to-run-flutter-app-on-ios-default-app-has-been-already-configured
+    // https://github.com/FirebaseExtended/flutterfire/issues/6433
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
