84e31633a5b3746e979f69f8c93e8db02b1e48ed
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-01-17 12:46:48 +0100

    Stage (#11)
    * script to create firebase secrets. and gitignore it
    
    * Update create_secrets.sh
    
    * Minor gitignore (#10)
    
    * setting thingsup. changes to get datasase defaultin dev database
    
    * made secret folder to gitignore
    
    * update import paths

---
+++ create_secrets.sh
@@ -0,0 +1,4 @@
+
+#https://stackoverflow.com/questions/67964110/how-to-access-secrets-when-using-flutter-web-with-github-actions
+
+echo $WMS_SECRETS_FIREBASE | base64 -d > ./lib/secrets/WMS_Secrets_Firebase.dart
