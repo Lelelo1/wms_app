439b5640221a65de455ff0ef8189f7209a988ebf
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-01-24 17:35:21 +0100

    adhoc to allow installing ios

--- release.sh
+++ release.sh
@@ -3,11 +3,10 @@
 
 #!/bin/bash
 
+flutter clean
 
-scripts/specific/release_android.sh
-scripts/specific/release_ios.sh
-
-set -e
+scripts/specific/release_ios.sh || { echo 'release ios failed' ; exit 1;}
+scripts/specific/release_android.sh || { echo 'release android failed' ; exit 1;}
 
 # Find and increment the version number.
 perl -i -pe 's/^(version:\s+\d+\.\d+\.)(\d+)(\+)(\d+)$/$1.($2+1).$3.($4+1)/e' pubspec.yaml
