2e1634e6794e82278913fa379d86a03ef9b54f8b
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-01-26 19:28:46 +0100

    Can use osm for very near places

--- prod.sh
+++ prod.sh
@@ -3,6 +3,6 @@
 
 # run wms_app with intended release featural behavior connected with real warehousystem (WARNING)
 
-#flutter run -d R58N11QMTCL --dart-define="CONFIGURATION=prod"
+flutter run -d R58N11QMTCL --dart-define="CONFIGURATION=prod"
 
 # I don't see why this woudl be needed though
