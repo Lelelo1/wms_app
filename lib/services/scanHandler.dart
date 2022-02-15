4ef005267bbb59f63a4afe4ff7491777de55857b
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-02-15 14:11:47 +0100

    extract shelf from shelf qr url

--- lib/services/scanHandler.dart
+++ lib/services/scanHandler.dart
@@ -2,13 +2,16 @@ import 'package:wms_app/models/product.dart';
 import 'package:wms_app/remote/WarehouseSystem.dart';
 import 'package:wms_app/stores/workStore.dart';
 
+import '../utils.dart';
+
 class ScanHandler {
   static late WarehouseSystem warehouseSystem = WarehouseSystem.instance;
 
   static void handleScanData(String scanData, Product currentlyScannedProduct,
       Function(Product product) barcodeHandler) async {
-    if (await _didScanQR(scanData)) {
-      print("was qr");
+    var asShelf = await _toQR(scanData);
+    if (asShelf.isNotEmpty) {
+      print("was qr: " + asShelf);
       if (currentlyScannedProduct.exists()) {
         print("increasing product amount");
         warehouseSystem.increaseAmountOfProducts(currentlyScannedProduct);
@@ -22,8 +25,14 @@ class ScanHandler {
     barcodeHandler(await warehouseSystem.fetchProduct(scanData));
   }
 
-  static Future<bool> _didScanQR(String scanData) async {
-    var shelfMatch = await warehouseSystem.findShelf(scanData);
-    return scanData == shelfMatch;
+  static Future<String> _toQR(String scanData) async {
+    try {
+      // removal of 'http://' part in wr code
+      var possibleShelf = scanData.split("//")[1];
+      var shelf = await warehouseSystem.findShelf(possibleShelf);
+      return shelf;
+    } catch (exc) {
+      return "";
+    }
   }
 }
