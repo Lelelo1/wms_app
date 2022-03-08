34019f02a01b814276bfd2a81f782e29196ed827
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-03-08 13:16:19 +0100

    Can increase amount of product with shelf scan

--- lib/services/scanHandler.dart
+++ lib/services/scanHandler.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:event/event.dart';
 import 'package:flutter/material.dart';
+import 'package:wms_app/models/attributes.dart';
 import 'package:wms_app/models/product.dart';
 import 'package:wms_app/remote/WarehouseSystem.dart';
 import 'package:wms_app/services/visionService.dart';
@@ -59,19 +60,23 @@ class ScanHandler {
     return false;
   }
 
-  static Future<bool> isMatchingShelf(String scanResult) async {
+// remake!
+  static Future<bool> isMatchingShelf(String shelf) async {
     var currentProduct = WorkStore.instance.currentProduct;
 
     if (!currentProduct.exists()) {
       return false;
     }
 
-    var shelf = await warehouseSystem.findShelf(scanResult);
+    var productEAN = await currentProduct.getEAN();
 
-    if (shelf.isEmpty) {
+    if (productEAN.isEmpty) {
       return false;
     }
-    return shelf == await currentProduct.getShelf();
+
+    var productShelf = await currentProduct.getShelf();
+
+    return shelf == productShelf;
   }
 
   static Future<Product> handleAsProduct(String scanResult) async {
