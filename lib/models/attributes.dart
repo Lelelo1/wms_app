48055e7c10c84f3ba8d76bea034700e27b4227b0
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-03-28 13:51:53 +0200

    added existing queries to warehouse json

--- lib/models/attributes.dart
+++ lib/models/attributes.dart
@@ -2,9 +2,9 @@ typedef Attribute = String;
 
 // Katsumi match attributes in MySql Magento warehouse system, "Attributkod"
 class Attributes {
-  static Attribute ean = "ean_code";
+  static Attribute ean = "ean_code"; //
   static Attribute images = "images";
-  static Attribute name = "name";
+  static Attribute name = "name"; //
   static Attribute sku = "sku";
-  static Attribute shelf = "c2c_hyllplats";
+  static Attribute shelf = "c2c_hyllplats"; //
 }
