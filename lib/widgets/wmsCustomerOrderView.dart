4f586e7322bcc6ddc9890523d5919bed7e50195f
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-09-05 14:51:31 +0200

    custom product view view refactor to have check box handerl custome rproduct item, faster render

--- lib/widgets/wmsCustomerOrderView.dart
+++ lib/widgets/wmsCustomerOrderView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wms_app/models/customerOrder.dart';
+import 'package:wms_app/models/customerOrderProduct.dart';
 import 'package:wms_app/models/product.dart';
 import 'package:wms_app/routes/productRoute.dart';
 import 'package:wms_app/views/extended/stacked.dart';
@@ -16,36 +17,35 @@ class WMSCustomerOrderView extends StatelessWidget {
     return Column(children: [
       Text(customerOrder.name,
           style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-      asyncCustomerOrderView(
-          Future.wait([
-            ...customerOrder.productId.map((id) => Product.fetchFromId(id))
-          ]),
-          context)
+      sizedProductViews(customerOrder.customerOrderProducts, context)
     ]);
   }
 
-  WMSAsyncWidget asyncCustomerOrderView(
-          Future<List<Product>> futureProducts, BuildContext context) =>
-      WMSAsyncWidget<List<Product>>(
-          futureProducts,
-          (products) => Column(children: [
-                ...products.map((p) => Card(
-                    child: Container(
-                        child: productInformation(p),
-                        width: MediaQuery.of(context).size.width * 0.85,
-                        height: MediaQuery.of(context).size.height * 0.85),
-                    margin: EdgeInsets.all(10)))
-              ]));
+  Widget sizedProductViews(List<CustomerOrderProduct> customerOrderProducts,
+          BuildContext context) =>
+      Column(children: [
+        ...customerOrderProducts.map((p) => Card(
+            child: Container(
+                child: productInformation(p),
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: MediaQuery.of(context).size.height * 0.85),
+            margin: EdgeInsets.all(10)))
+      ]);
 
-  Widget productInformation(Product product) {
+  Widget productInformation(CustomerOrderProduct customerOrderProduct) {
     return Column(children: [
-      ProductInformationWidgets.titleArea(product.sku),
-      ProductInformationWidgets.subtitleArea(
-          product.id.toString(), product.ean.toString()),
-      Image.network(product.frontImage),
-      ProductInformationWidgets.bottomArea(product.shelf, product.qty),
-      ProductInformationWidgets.nameWidget(product.name),
-      Checkbox(value: true, onChanged: (b) {})
+      WMSAsyncWidget<Product>(
+          Product.fetchFromId(customerOrderProduct.productId),
+          (product) => Column(children: [
+                ProductInformationWidgets.titleArea(product.sku),
+                ProductInformationWidgets.subtitleArea(
+                    product.id.toString(), product.ean.toString()),
+                Image.network(product.frontImage),
+                ProductInformationWidgets.bottomArea(
+                    product.shelf, product.qty),
+                ProductInformationWidgets.nameWidget(product.name)
+              ])),
+      Checkbox(value: true, onChanged: (value) {})
     ]);
   }
 }
